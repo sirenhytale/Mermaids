c7f8c4c02d2106357103111b0b0578680373b821
meFroggy <SubToFroggy@gmail.com>
2026-03-25 18:52:41 -0400

    Ignored

--- src/main/java/plugin/siren/Utils/Github/Ignore.java
+++ src/main/java/plugin/siren/Utils/Github/Ignore.java
@@ -2,7 +2,6 @@ package plugin.siren.Utils.Github;
 
 public class Ignore {
     private static String HStatsModUUID = "8aab16df-13b6-429e-9568-9b80effacdc8";
-
     public static String getHStatsModUUID(){
         return HStatsModUUID;
     }
